fa32cc53c1d4ec5657c07ad03a6a651fb24915ab
guidoffm <guidoffm@users.noreply.github.com>
2025-02-06 23:49:40 +0100

    Update dmx-send.ts

--- examples/dmx-send.ts
+++ examples/dmx-send.ts
@@ -1,7 +1,6 @@
 import { OlaClient } from "../src/index";
 
 (async () => {
-    console.log("Hello via Bun!");
 
     async function sleep(ms: number) {
         return new Promise((resolve) => setTimeout(resolve, ms));
